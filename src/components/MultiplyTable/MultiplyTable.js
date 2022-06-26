7b2a40b1393436c8ec659a10e406839a888a2d60
Anton Valkovsky <ruizave@gmail.com>
2022-06-26 18:03:46 +0300

    добавлены идентификаторы ряда

--- src/components/MultiplyTable/MultiplyTable.js
+++ src/components/MultiplyTable/MultiplyTable.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MultiplyTable.css'
+import * as PropTypes from "prop-types";
+
+function RowDigit(props) {
+
+  return (
+    <div id={'id-'+props.row+'-'+props.col} className={props.classString} data-value={props.dataValue} >
+      {props.dataValue}
+    </div>);
+}
+
+RowDigit.propTypes = {dataValue: PropTypes.any};
 
 function MultiplyTable({valueArray}) {
+
   return (
     <div className="mult">
 
-      {valueArray.map((v1,i1) => {
-          return (
-            <div className="main-row" data-row={i1 + 1}>
-              <div className="digit-item-number">{i1 + 1}</div>
-              {v1.map((v2) => <div className="digit-item" data-value={v2}>{v2}</div>)}
-            </div>)
-        })
+      {valueArray.map((v1, i1) => {
+        return (
+          <div className="main-row" data-row={i1 + 1}>
+            <div className="digit-item-number">{i1 + 1}</div>
+            {v1.map((v2) => <RowDigit  dataValue={v2} classString='digit-item' col={v2} row={i1 + 1}/>)}
+          </div>)
+      })
       }
 
 
