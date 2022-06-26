4941c8be0e71e2474849f59b148fa38399e48a64
Anton Valkovsky <ruizave@gmail.com>
2022-06-26 15:41:40 +0300

    реализовано базовое умножение

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import {useEffect, useState} from "react";
 
+
 function App() {
 
   const [factor1,setFactor1] = useState(0);
   const [factor2,setFactor2] = useState(0);
   const [result,setResult] = useState(0)
+  const [multiplyTable,setMultiplyTable] = useState([]);
 
   useEffect(()=>{
     setFactor1(Math.floor(Math.random()*10));
     setFactor2(Math.floor(Math.random()*10));
+    setMultiplyTable(()=>{
+      const masterArray = new Array(10);
+      masterArray.map(value => new Array(10));
+    })
   },[])
 
   useEffect(()=>{
